a3b0494dd5a7b18d9e94611e370b810c529c239a
babuharsh19 <babuharsh19@gmail.com>
2023-08-15 20:23:03 +0530

    html completed

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-console.log("Hello !");
-//let -> block scoped variables 
-let a=5
-console.log(a); 
+// // console.log("Hello !");
+// //let -> block scoped variables 
+// let a;
+// let firstName = "Harsh";
+// let lastName = "Babu";
+// console.log(windows.location.hostname); 
+
+function makeClickableLink(link) {
+    // Check if the link is a valid URL.
+    if (!/^https?:\/\//.test(link)) {
+      return link;
+    }
+  
+    // Create the HTML anchor element.
+    // const anchorElement = document.createElement("a");
+    anchorElement.href = link;
+    anchorElement.textContent = link;
+  
+    // Return the anchor element.
+    return anchorElement;
+  }
+  
+  // Example usage.
+  const link = "https://www.example.com";
+  const clickableLink = makeClickableLink(link);
+  
+  console.log(clickableLink); 
